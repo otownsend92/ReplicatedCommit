42461ffa3db7b6ecec358308f14734ee835cad20
alickrxu <alickrxu@gmail.com>
2016-02-27 22:55:53 -0800

    changed transaction type to string

--- src/Transaction.java
+++ src/Transaction.java
@@ -2,19 +2,19 @@
  * Simple class that represents a Transaction
  */
 public class Transaction {
-    private char type; 
+    private String type; 
     private String variable;
     private int writeValue;
 
-    public Transaction(char type, String variable, int writeValue) {
+    public Transaction(String type, String variable, int writeValue) {
         this.type = type;
         this.variable = variable;
         this.writeValue = writeValue;
     }
 
-    public boolean isRead() { return type == 'r'; }
+    public boolean isRead() { return type.equals("r"); }
 
-    public char getType() { return type; }
+    public String getType() { return type; }
 
     public String getVariable() { return variable; }
 
